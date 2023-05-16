ecf03db37e6e413ec403ec97a04d8c34ff099f8b
FernanVaz <130450605+FernanVaz@users.noreply.github.com>
2023-05-16 10:17:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
-package com.softtek.modelo;
+package com.softtek.presentacion;
 
-public class campos {
-        public int x=10;
-        public int muestra() {
-            return x;
-        }
-        public void incrementa() {
-            x++;
-        }
+import com.softtek.modelo.campos;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        campos c = new campos(); // Creamos una instancia de la clase Campos
+        System.out.println(c.muestra()); // Imprime el valor actual de x (0)
+        c.incrementa();
+        c.incrementa();
+        c.incrementa();// Incrementa el valor de x en 1
+        System.out.println(c.muestra()); // Imprime el nuevo valor de x (1)
+    }
 }
+
+
